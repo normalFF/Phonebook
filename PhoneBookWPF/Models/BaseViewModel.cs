4154f41fe27ceb9197847a0d3639635cd1b9d688
fgvngfdf <fgvngfdf@gmail.com>
2022-02-22 13:35:00 +0200

    Подготовка проекта

---
+++ PhoneBookWPF/Models/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PhoneBookWPF.Models
+{
+	internal abstract class BaseViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public bool Set<T>(ref T currentValue, T newValue, [CallerMemberName] string parameter = null)
+		{
+			if (Equals(currentValue, newValue)) return false;
+			currentValue = newValue;
+			OnPropertyChanged(parameter);
+			return true;
+		}
+
+		public void OnPropertyChanged([CallerMemberName] string parameter = null)
+		{
+			PropertyChanged.Invoke(this, new PropertyChangedEventArgs(parameter));
+		}
+	}
+}
