62def43b0a2abd459f48d6d7f6e4fac7a41e4823
fgvngfdf <fgvngfdf@gmail.com>
2022-02-24 23:20:40 +0200

    Добавлены поля и свойства для вкладки "Добавление абонента"

--- PhoneBookWPF/Models/MainViewModel.cs
+++ PhoneBookWPF/Models/MainViewModel.cs
@@ -1,16 +1,30 @@
-﻿using LibraryOOP;
+﻿using System;
+using LibraryOOP;
 using System.Collections.Generic;
 
 namespace PhoneBookWPF.Models
 {
 	internal class MainViewModel : BaseViewModel
 	{
-		private PhoneType _main, _home, _work;
 		private PhoneBook _phoneBook;
 		private List<Abonent> _abonents;
 		private List<AbonentsGroup> _abonentsGroups;
 		private Abonent _selectedAbonent;
 		private AbonentsGroup _selectedAbonentsGroup;
+
+		private string _nameCreate;
+		private string _surnameCreate;
+		private string _residentCreate;
+		private DateTime? _dateTimeCreate;
+		private List<PhoneNumber> _phonesMainCreate;
+		private string _phoneMainAdd;
+		private PhoneNumber _phoneMainSelect;
+		private List<PhoneNumber> _phonesHomeCreate;
+		private string _phoneHomeAdd;
+		private PhoneNumber _phoneHomeSelect;
+		private List<PhoneNumber> _phonesWorkCreate;
+		private string _phoneWorkAdd;
+		private PhoneNumber _phoneWorkSelect;
 
 		public List<Abonent> Abonents
 		{
@@ -32,9 +46,72 @@ namespace PhoneBookWPF.Models
 			get => _selectedAbonentsGroup;
 			set => Set(ref _selectedAbonentsGroup, value);
 		}
-		public PhoneType Main => _main;
-		public PhoneType Home => _home;
-		public PhoneType Work => _work;
+
+		public string NameCreate
+		{
+			get => _nameCreate;
+			set => Set(ref _nameCreate, value);
+		}
+		public string SurnameCreate
+		{
+			get => _surnameCreate;
+			set => Set(ref _surnameCreate, value);
+		}
+		public string ResidentCreate
+		{
+			get => _residentCreate;
+			set => Set(ref _residentCreate, value);
+		}
+		public DateTime? DateTimeCreate
+		{
+			get => _dateTimeCreate;
+			set => Set(ref _dateTimeCreate, value);
+		}
+		public List<PhoneNumber> PhonesMainCreate
+		{
+			get => _phonesMainCreate;
+			set => Set(ref _phonesMainCreate, value);
+		}
+		public string PhoneMainAdd
+		{
+			get => _phoneMainAdd;
+			set => Set(ref _phoneMainAdd, value);
+		}
+		public PhoneNumber PhoneMainSelect
+		{
+			get => _phoneMainSelect;
+			set => Set(ref _phoneMainSelect, value);
+		}
+		public List<PhoneNumber> PhonesHomeCreate
+		{
+			get => _phonesHomeCreate;
+			set => Set(ref _phonesHomeCreate, value);
+		}
+		public string PhoneHomeAdd
+		{
+			get => _phoneHomeAdd;
+			set => Set(ref _phoneHomeAdd, value);
+		}
+		public PhoneNumber PhoneHomeSelect
+		{
+			get => _phoneHomeSelect;
+			set => Set(ref _phoneHomeSelect, value);
+		}
+		public List<PhoneNumber> PhonesWorkCreate
+		{
+			get => _phonesWorkCreate;
+			set => Set(ref _phonesWorkCreate, value);
+		}
+		public string PhoneWorkAdd
+		{
+			get => _phoneWorkAdd;
+			set => Set(ref _phoneWorkAdd, value);
+		}
+		public PhoneNumber PhoneWorkSelect
+		{
+			get => _phoneWorkSelect;
+			set => Set(ref _phoneWorkSelect, value);
+		}
 
 		public MainViewModel()
 		{
@@ -42,9 +119,8 @@ namespace PhoneBookWPF.Models
 			GetData.GetListAbonents(_phoneBook, 20);
 			Abonents = _phoneBook.Abonents;
 			AbonentsGroups = _phoneBook.AbonentsGroups;
-			_main = PhoneType.Личный;
-			_home = PhoneType.Домашний;
-			_work = PhoneType.Рабочий;
 		}
+
+
 	}
 }
