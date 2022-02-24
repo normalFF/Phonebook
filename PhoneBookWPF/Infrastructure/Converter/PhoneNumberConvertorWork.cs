51ecca021ef02a73dcd48bfb821a35a6ad0a7211
fgvngfdf <fgvngfdf@gmail.com>
2022-02-24 22:34:13 +0200

    Классы конверторы

---
+++ PhoneBookWPF/Infrastructure/Converter/PhoneNumberConvertorWork.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using LibraryOOP;
+
+namespace PhoneBookWPF.Infrastructure.Converter
+{
+	internal class PhoneNumberConvertorWork : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is List<PhoneNumber> numbers)
+			{
+				string returnString = "";
+				foreach (var item in numbers)
+				{
+					if ((int)item.Type == 0)
+					{
+						if (returnString.Length == 0)
+						{
+							returnString += $"{item.Phone}";
+						}
+						else
+						{
+							returnString += $"\n{item.Phone}";
+						}
+					}
+				}
+				if (returnString.Length == 0) return "Отсутствует";
+				return returnString;
+			}
+
+			return "Отсутствует";
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
