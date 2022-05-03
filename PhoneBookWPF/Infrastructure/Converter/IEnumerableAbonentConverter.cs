f48916431ed47e80f6ad637788a2c7a053b51f4b
fgvngfdf <fgvngfdf@gmail.com>
2022-05-04 01:28:50 +0300

    Добавлен конвертер для конвертации из IEnumerable<T> в List<T>

---
+++ PhoneBookWPF/Infrastructure/Converter/IEnumerableAbonentConverter.cs
@@ -0,0 +1,23 @@
+﻿using LibraryOOP;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace PhoneBookWPF.Infrastructure.Converter
+{
+	internal class IEnumerableAbonentConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var collection = value as IEnumerable<Abonent>;
+			return collection.ToList();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
