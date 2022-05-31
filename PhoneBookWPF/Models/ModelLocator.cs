735cd3a7575502142fbbf34a7da93639ac78fb23
fgvngfdf <fgvngfdf@gmail.com>
2022-06-01 01:38:45 +0300

    Добавлена модель для получения моделей представлений.

---
+++ PhoneBookWPF/Models/ModelLocator.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PhoneBookWPF.Models
+{
+	internal class ModelLocator
+	{
+		public MainViewModel MainView => App.Host.Services.GetRequiredService<MainViewModel>();
+
+		public AbonentAddViewModel AbonentView => App.Host.Services.GetRequiredService<AbonentAddViewModel>();
+	}
+}
