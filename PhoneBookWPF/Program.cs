ca9022248f34619822d894ee12bb9b2582ab5690
fgvngfdf <fgvngfdf@gmail.com>
2022-06-01 01:36:50 +0300

    Точка входа в приложение. Добавлен метод создания хоста приложения.

---
+++ PhoneBookWPF/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace PhoneBookWPF
+{
+	public static class Program
+	{
+		[STAThread]
+		public static void Main()
+		{
+			var application = new App();
+			application.InitializeComponent();
+			application.Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] Args)
+		{
+			IHostBuilder host = Host.CreateDefaultBuilder(Args);
+
+			host.UseContentRoot(Environment.CurrentDirectory);
+			host.ConfigureAppConfiguration((host, cfg) =>
+			{
+				cfg.SetBasePath(Environment.CurrentDirectory);
+			});
+			host.ConfigureServices(App.ConfigureServices);
+
+			return host;
+		}
+	}
+}
