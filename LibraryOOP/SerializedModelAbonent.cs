c4db7cdfe0540e8995d2041addc50d3b5f277ccc
fgvngfdf <fgvngfdf@gmail.com>
2022-06-01 01:26:48 +0300

    Добавлены модели классов Abonent и Phone для сериализации данных.

---
+++ LibraryOOP/SerializedModelAbonent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LibraryOOP
+{
+	[Serializable]
+	class SerializedModelAbonent
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string DateOfBirth { get; set; }
+		public string Residence { get; set; }
+		public SerializedModelPhone[] Phones { get; set; }
+		public string[] Groups { get; set; }
+	}
+}
---
+++ LibraryOOP/SerializedModelPhone.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LibraryOOP
+{
+	[Serializable]
+	class SerializedModelPhone
+	{
+		public string Type { get; set; }
+		public string Phone { get; set; }
+	}
+}
